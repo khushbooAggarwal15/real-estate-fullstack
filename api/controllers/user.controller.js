597324e2b52df18f29b00ea28acecb74f100393a
khushboo <aggarwal.khushi1501@gmail.com>
2024-08-25 18:58:06 +0530

    user update user profile

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,14 +1,12 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 export const updateUser = async (req, res, next) => {
-  console.log(req.user.id != req.params.id, req.params.id, req.user.id);
   if (req.user.id !== req.params.id)
-    return res
-      .status(403)
-      .json({ message: "You are not authorized to update this user" });
+    return next(errorHandler(401, "You can only update your own account!"));
   try {
     if (req.body.password) {
-      req.body.password = bcryptjs.hashSync(req.body.passsword, 10);
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
     const updateUser = await User.findByIdAndUpdate(
       req.params.id,
@@ -23,11 +21,7 @@ export const updateUser = async (req, res, next) => {
       { new: true }
     );
     const { passsword: pass, ...rest } = updateUser._doc;
-    res.status(201).json({
-      success: true,
-      message: "User updated successfully",
-      user: rest,
-    });
+    res.status(201).json(rest);
   } catch (err) {
     next(err);
   }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,13 +9,21 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/user/userSlice.js";
 const Profile = () => {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
   const [filePercentage, setFilePercentage] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
   useEffect(() => {
     if (file) {
       handleFileUpload(file);
@@ -51,10 +59,37 @@ const Profile = () => {
       }
     );
   };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (err) {
+      dispatch(updateUserFailure(err.message));
+    }
+  };
   return (
     <div className="max-w-lg p-3 mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -86,27 +121,41 @@ const Profile = () => {
           type="text"
           className="border p-3 rounded-lg"
           id="username"
+          defaultValue={currentUser.username}
+          onChange={handleChange}
         />
         <input
           placeholder="email"
           type="email"
           className="border p-3 rounded-lg"
           id="email"
+          defaultValue={currentUser.email}
+          onChange={handleChange}
         />
         <input
           placeholder="password"
           type="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Update
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading" : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      {updateSuccess ? (
+        <p className="text-green-700 mt-5">User Updated Succesfully</p>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
